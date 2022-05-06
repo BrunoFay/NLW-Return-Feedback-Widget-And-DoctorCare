7e458731a12bcea95f3ae9d0247efa4c955b138e
BRUNO <brunofay1@hotmail.com>
2022-05-06 20:23:40 -0300

    TEST

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-  
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+},
+withCredentials: true
 })
