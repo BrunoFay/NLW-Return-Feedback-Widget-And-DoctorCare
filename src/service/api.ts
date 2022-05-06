c53c63ee9ee590a3f1fb89b9464330912d41dc9b
BRUNO <brunofay1@hotmail.com>
2022-05-06 18:39:04 -0300

    TEST change url env to string

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:import.meta.env.VITE_API_URL
+  baseURL:'https://nlw-feedback-widget-6dag3dfzr-brunofay.vercel.app/'
+  
 })
+//baseURL:import.meta.env.VITE_API_URL
