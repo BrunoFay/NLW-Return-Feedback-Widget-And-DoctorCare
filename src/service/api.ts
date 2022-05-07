c20f9f9c105f997590132ee644dda5fba9e203b4
BRUNO <brunofay1@hotmail.com>
2022-05-06 21:20:58 -0300

    TEST

--- src/service/api.ts
+++ src/service/api.ts
@@ -4,7 +4,7 @@ export const api = axios.create({
   baseURL: 'https://nlw-feedbackwedget-backend.herokuapp.com/',
   withCredentials: false,
   headers: {
-    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Origin' : 'https://nlw-feedback-widget-1l32u0v6a-brunofay.vercel.app/',
     'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     'Content-Type': 'application/json'
     }
